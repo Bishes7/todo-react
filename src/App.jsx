2566ac5e64d80bb6f54ad9d409da502c0b1dc59d
Bishes Adhikari <specialbishes9@gmail.com>
2024-12-30 23:44:45 +1100

    added javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const handleonSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim()) {
+      setList([...list, task]);
+      setTask("");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <form onSubmit={handleonSubmit}>
+          <input
+            type="text"
+            value={task}
+            placeholder="Add Task"
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <div className="list">
+          <ul>
+            {list.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
