d8ccc6cb45c7a90f123a7316966bb1524a0a6e1c
Bishes Adhikari <specialbishes9@gmail.com>
2025-01-01 23:56:47 +1100

    delete and toggle functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,21 @@ function App() {
   const handleonSubmit = (e) => {
     e.preventDefault();
     if (task.trim()) {
-      setList([...list, task]);
+      setList([...list, { text: task, isCompleted: false }]);
       setTask("");
     }
   };
 
   const handleOnDelete = (index) => {
     const updatedList = list.filter((item, i) => i !== index);
+    setList(updatedList);
+  };
+
+  // Add a toggle Completed function
+  const toggleCompleted = (index) => {
+    const updatedList = list.map((item, i) =>
+      i === index ? { ...item, isCompleted: !item.isCompleted } : item
+    );
     setList(updatedList);
   };
 
@@ -33,8 +41,16 @@ function App() {
         <div className="list">
           <ul>
             {list.map((item, i) => (
-              <li key={i}>
-                {item}
+              <li
+                key={i}
+                style={{
+                  textDecoration: item.isCompleted ? "line-through" : "none",
+                }}
+              >
+                {item.text}
+                <button onClick={() => toggleCompleted(i)}>
+                  {item.isCompleted ? "undo" : "completed"}
+                </button>
                 <button onClick={() => handleOnDelete(i)}>Delete</button>
               </li>
             ))}
